cd14d0326704b073d6152d0f3dcc73d54682bf0c
Nickolai <colya.polischuk@yandex.ru>
2023-04-11 19:59:33 +0300

    social network layout

---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img className="img-hat" src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg" />
+      </div>
+      <div>
+        AVA
+      </div>
+      <div>
+        My posts
+        <div>
+          New Post
+        </div>
+        <div>
+          <div>
+            Post 1
+          </div>
+          <div>
+            Post 2
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
