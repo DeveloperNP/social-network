d7a2e6c8fe44e3fd68640bff4cc2d92e8d5badcf
Nickolai <colya.polischuk@yandex.ru>
2023-04-12 22:22:20 +0300

    added CSS modules (14th lesson)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import s from './Header.module.css'
 
 const Header = () => {
   return (
-    <header className='header'>
+    <header className={s.header}>
       <img src='https://audimediacenter-a.akamaihd.net/system/production/media/1282/images/bde751ee18fe149036c6b47d7595f6784f8901f8/AL090142_full.jpg?1581961854' />
     </header>
   );
