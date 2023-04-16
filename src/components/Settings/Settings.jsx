8a104c6af055868e105c3eb49c71cffa74e3bc00
Nickolai <colya.polischuk@yandex.ru>
2023-04-16 23:45:02 +0300

    added switching between tabs with react-router-dom module, added new components (19th lesson)

---
+++ src/components/Settings/Settings.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Settings.module.css'
+
+const Settings = () => {
+  return (
+    <div className={s.settingsHeight}>
+      Settings
+    </div>
+  );
+}
+
+export default Settings;
