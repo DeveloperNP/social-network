5e0f1a5cc8394d36be8814944e3e2314831dec51
Nickolai <colya.polischuk@yandex.ru>
2023-04-18 20:51:00 +0300

    <Dialogs /> layout added (21st lesson)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,39 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
   return (
-    <div className={s.dialogsHeight}>
-      Dialogs
+    <div className={s.dialogsWrapper}>
+      <div className={s.dialogs}>
+        <div className={`${s.dialog} ${s.active}`}>
+          Nickolai
+        </div>
+        <div className={s.dialog}>
+          Andrey
+        </div>
+        <div className={s.dialog}>
+          Kristina
+        </div>
+        <div className={s.dialog}>
+          Lyonya
+        </div>
+        <div className={s.dialog}>
+          Irina
+        </div>
+        <div className={s.dialog}>
+          Tatiana
+        </div>
+        <div className={s.dialog}>
+          Julia
+        </div>
+        <div className={s.dialog}>
+          Diana
+        </div>
+      </div>
+      <div className={s.messages}>
+        <div className={s.message}>Hi, how are you?</div>
+        <div className={s.message}>I'm bored :c</div>
+        <div className={s.message}>The weather is cloudy today</div>
+        <div className={s.message}>Let's play together!!!</div>
+      </div>
     </div>
   );
 }
