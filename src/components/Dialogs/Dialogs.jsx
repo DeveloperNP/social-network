f0e7f3b1c30a0a48c39fb001e129f39775b16eca
Nickolai <colya.polischuk@yandex.ru>
2023-04-22 21:49:01 +0300

    refactoring of <Profile /> and <Dialogs /> components (23rd lesson)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,56 +2,45 @@ import React from "react";
 import s from './Dialogs.module.css'
 import { NavLink } from "react-router-dom";
 
+const DialogItem = (props) => {
+  const path = `/dialogs/${props.id}`;
+
+  return (
+    <div className={s.item}>
+      <NavLink to={path} className={navData => navData.isActive ? s.activeDialog : s.dialog}>
+        {props.name}
+      </NavLink>
+    </div>
+  );
+}
+
+const MessageItem = (props) => {
+  return (
+    <div className={s.message}>
+      {props.message}
+    </div>
+  );
+}
+
 const Dialogs = () => {
   return (
     <div className={s.dialogsWrapper}>
       <div className={s.dialogs}>
-        <div className={s.item}>
-          <NavLink to="/dialogs/1" className={navData => navData.isActive ? s.activeDialog : s.dialog}>
-            Nickolai
-          </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/2" className={navData => navData.isActive ? s.activeDialog : s.dialog}>
-            Andrey
-          </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/3" className={navData => navData.isActive ? s.activeDialog : s.dialog}>
-            Kristina
-          </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/4" className={navData => navData.isActive ? s.activeDialog : s.dialog}>
-            Lyonya
-          </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/5" className={navData => navData.isActive ? s.activeDialog : s.dialog}>
-            Irina
-          </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/6" className={navData => navData.isActive ? s.activeDialog : s.dialog}>
-            Tatiana
-          </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/7" className={navData => navData.isActive ? s.activeDialog : s.dialog}>
-            Julia
-          </NavLink>
-        </div>
-        <div className={s.item}>
-          <NavLink to="/dialogs/8" className={navData => navData.isActive ? s.activeDialog : s.dialog}>
-            Diana
-          </NavLink>
-        </div>
+        <DialogItem id="1" name="Nickolai" />
+        <DialogItem id="2" name="Andrey" />
+        <DialogItem id="3" name="Kristina" />
+        <DialogItem id="4" name="Lyonya" />
+        <DialogItem id="5" name="Irina" />
+        <DialogItem id="6" name="Tatiana" />
+        <DialogItem id="7" name="Julia" />
+        <DialogItem id="8" name="Diana" />
       </div>
       <div className={s.messages}>
-        <div className={s.message}>Hi, how are you?</div>
-        <div className={s.message}>I'm bored :c</div>
-        <div className={s.message}>The weather is cloudy today</div>
-        <div className={s.message}>Let's play together!!!</div>
+        <MessageItem message="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus iste maiores error? Suscipit quo, maiores dolores quidem odit ducimus nulla voluptas atque." />
+        <MessageItem message="Hi, how are you?" />
+        <MessageItem message="I'm bored :c" />
+        <MessageItem message="The weather is cloudy today" />
+        <MessageItem message="Let's play together!!!" />
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,18 +5,18 @@ import Post from "./Post/Post";
 const MyPosts = () => {
   return (
     <div className={s.posts}>
-      <div>MY POSTS</div>
-      <div className={s.newPost}>
-        <textarea></textarea>
-        <button>Add post</button>
+      <div className={s.postsHeader}>
+        <h3>MY POSTS</h3>
+        <div className={s.newPost}>
+          <textarea></textarea>
+          <button>Add post</button>
+        </div>
       </div>
-      <div>
-        <Post message="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!" likesCount="508"/>
-        <Post message="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!" likesCount="287"/>
-        <Post message="The weather is fine today" likesCount="35"/>
-        <Post message="How are you?" likesCount="58"/>
-        <Post message="Hello, World! This is my first post ^.^" likesCount="104"/>
-      </div>
+      <Post message="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!" likesCount="508" />
+      <Post message="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!" likesCount="287" />
+      <Post message="The weather is fine today" likesCount="35" />
+      <Post message="How are you?" likesCount="58" />
+      <Post message="Hello, World! This is my first post ^.^" likesCount="104" />
     </div>
   );
 }
