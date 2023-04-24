10b53fff03251ae161ba89335d721c78ab152f8c
Nickolai <colya.polischuk@yandex.ru>
2023-04-24 23:17:49 +0300

    refactoring of <Dialogs /> and <MyPosts /> - added new arrays using .map (25th lesson)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -23,43 +23,35 @@ const MessageItem = (props) => {
 }
 
 const Dialogs = () => {
-  let dialogsData = [
-    {id: 1, name: 'Nickolai'},
-    {id: 2, name: 'Andrey'},
-    {id: 3, name: 'Kristina'},
-    {id: 4, name: 'Lyonya'},
-    {id: 5, name: 'Irina'},
-    {id: 6, name: 'Tania'},
-    {id: 7, name: 'Julia'},
-    {id: 8, name: 'Diana'}
+  let dialogs = [
+    { id: 1, name: 'Nickolai' },
+    { id: 2, name: 'Andrey' },
+    { id: 3, name: 'Kristina' },
+    { id: 4, name: 'Lyonya' },
+    { id: 5, name: 'Irina' },
+    { id: 6, name: 'Tania' },
+    { id: 7, name: 'Julia' },
+    { id: 8, name: 'Diana' }
   ];
 
-  let messagesData = [
-    {id: 1, message: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus iste maiores error? Suscipit quo, maiores dolores quidem odit ducimus nulla voluptas atque.'},
-    {id: 2, message: 'Hi, how are you?'},
-    {id: 3, message: 'I\'m bored :c'},
-    {id: 4, message: 'The weather is cloudy today'},
-    {id: 5, message: 'Let\'s play together!!!'}
+  let messages = [
+    { id: 1, message: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus iste maiores error? Suscipit quo, maiores dolores quidem odit ducimus nulla voluptas atque.' },
+    { id: 2, message: 'Hi, how are you?' },
+    { id: 3, message: 'I\'m bored :c' },
+    { id: 4, message: 'The weather is cloudy today' },
+    { id: 5, message: 'Let\'s play together!!!' }
   ];
-  
+
+  let dialogsElements = dialogs.map(d => <DialogItem id={d.id} name={d.name} />);
+  let messagesElements = messages.map(m => <MessageItem message={m.message} />);
+
   return (
     <div className={s.dialogsWrapper}>
       <div className={s.dialogs}>
-        <DialogItem id={dialogsData[0].id} name={dialogsData[0].name} />
-        <DialogItem id={dialogsData[1].id} name={dialogsData[1].name} />
-        <DialogItem id={dialogsData[2].id} name={dialogsData[2].name} />
-        <DialogItem id={dialogsData[3].id} name={dialogsData[3].name} />
-        <DialogItem id={dialogsData[4].id} name={dialogsData[4].name} />
-        <DialogItem id={dialogsData[5].id} name={dialogsData[5].name} />
-        <DialogItem id={dialogsData[6].id} name={dialogsData[6].name} />
-        <DialogItem id={dialogsData[7].id} name={dialogsData[7].name} />
+        {dialogsElements}
       </div>
       <div className={s.messages}>
-        <MessageItem message={messagesData[0].message} />
-        <MessageItem message={messagesData[1].message} />
-        <MessageItem message={messagesData[2].message} />
-        <MessageItem message={messagesData[3].message} />
-        <MessageItem message={messagesData[4].message} />
+        {messagesElements}
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,14 +3,16 @@ import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
 const MyPosts = () => {
-  let postData = [
-    {id: 1, message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!', likesCount: 508},
-    {id: 2, message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!', likesCount: 287},
-    {id: 3, message: 'The weather is fine today', likesCount: 35},
-    {id: 4, message: 'How are you?', likesCount: 58},
-    {id: 5, message: 'Hello, World! This is my first post ^.^', likesCount: 104}
+  let posts = [
+    { id: 1, message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!', likesCount: 508 },
+    { id: 2, message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!', likesCount: 287 },
+    { id: 3, message: 'The weather is fine today', likesCount: 35 },
+    { id: 4, message: 'How are you?', likesCount: 58 },
+    { id: 5, message: 'Hello, World! This is my first post ^.^', likesCount: 104 }
   ];
-  
+
+  let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount} />);
+
   return (
     <div className={s.posts}>
       <div className={s.postsHeader}>
@@ -20,11 +22,7 @@ const MyPosts = () => {
           <button>Add post</button>
         </div>
       </div>
-      <Post message={postData[0].message} likesCount={postData[0].likesCount} />
-      <Post message={postData[1].message} likesCount={postData[1].likesCount} />
-      <Post message={postData[2].message} likesCount={postData[2].likesCount} />
-      <Post message={postData[3].message} likesCount={postData[3].likesCount} />
-      <Post message={postData[4].message} likesCount={postData[4].likesCount} />
+      {postsElements}
     </div>
   );
 }
