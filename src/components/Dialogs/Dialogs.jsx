4eb3a39eb9ca009713fccc4d73e69eb269e3e3d6
Nickolai <colya.polischuk@yandex.ru>
2023-04-26 22:50:18 +0300

    separation of UI and BLL: all data in index.js (27th lesson)

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,28 +3,9 @@ import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import MessageItem from "./MessageItem/MessageItem";
 
-const Dialogs = () => {
-  let dialogs = [
-    { id: 1, name: 'Nickolai' },
-    { id: 2, name: 'Andrey' },
-    { id: 3, name: 'Kristina' },
-    { id: 4, name: 'Lyonya' },
-    { id: 5, name: 'Irina' },
-    { id: 6, name: 'Tania' },
-    { id: 7, name: 'Julia' },
-    { id: 8, name: 'Diana' }
-  ];
-
-  let messages = [
-    { id: 1, message: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus iste maiores error? Suscipit quo, maiores dolores quidem odit ducimus nulla voluptas atque.' },
-    { id: 2, message: 'Hi, how are you?' },
-    { id: 3, message: 'I\'m bored :c' },
-    { id: 4, message: 'The weather is cloudy today' },
-    { id: 5, message: 'Let\'s play together!!!' }
-  ];
-
-  let dialogsElements = dialogs.map(d => <DialogItem id={d.id} name={d.name} />);
-  let messagesElements = messages.map(m => <MessageItem message={m.message} />);
+const Dialogs = (props) => {
+  let dialogsElements = props.dialogs.map(d => <DialogItem id={d.id} name={d.name} />);
+  let messagesElements = props.messages.map(m => <MessageItem message={m.message} />);
 
   return (
     <div className={s.dialogsWrapper}>
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,16 +2,8 @@ import React from "react";
 import s from './MyPosts.module.css'
 import Post from "./Post/Post";
 
-const MyPosts = () => {
-  let posts = [
-    { id: 1, message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!', likesCount: 508 },
-    { id: 2, message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!', likesCount: 287 },
-    { id: 3, message: 'The weather is fine today', likesCount: 35 },
-    { id: 4, message: 'How are you?', likesCount: 58 },
-    { id: 5, message: 'Hello, World! This is my first post ^.^', likesCount: 104 }
-  ];
-
-  let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount} />);
+const MyPosts = (props) => {
+  let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount} />);
 
   return (
     <div className={s.posts}>
