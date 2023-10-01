78f138a7dff37eae2d4b03c7f8731abc7c518372
Nickolai <colya.polischuk@yandex.ru>
2023-10-01 04:35:54 +0300

    added CSS for Users page

--- src/components/Users/UserItem/UserItem.jsx
+++ src/components/Users/UserItem/UserItem.jsx
@@ -3,35 +3,35 @@ import s from './UserItem.module.css'
 
 const UserItem = (props) => {
   return (
-    <div>
-      <span className={s.inlineBlock}>
+    <div className={s.userItemWrapper}>
+      <div className={s.avaAndSubBlock}>
         <div>
           <img src={props.avatar} alt='AVA' className={s.avatar} />
         </div>
         <div>
           {
             props.followed
-              ? <button onClick={() => { props.unfollow(props.id) }}>Unfollow</button>
-              : <button onClick={() => { props.follow(props.id) }}>Follow</button>
+              ? <button className={s.subButton} onClick={() => { props.unfollow(props.id) }}>Unfollow</button>
+              : <button className={s.subButton} onClick={() => { props.follow(props.id) }}>Follow</button>
           }
         </div>
-      </span>
-      <span className={s.inlineBlock}>
-        <div>
+      </div>
+      <div className={s.userInfoBlock}>
+        <div className={s.firstName}>
           {props.firstName}
         </div>
-        <div>
+        <div className={s.status}>
           {props.status}
         </div>
-      </span>
-      <span className={s.inlineBlock}>
+      </div>
+      <div className={s.locationBlock}>
         <div>
           {props.location.country}
         </div>
         <div>
           {props.location.city}
         </div>
-      </span>
+      </div>
     </div>
   );
 }
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,32 +1,44 @@
 import React from "react";
 import UserItem from "./UserItem/UserItem";
+import s from "./Users.module.css"
 
 const Users = (props) => {
   if (props.users.length === 0) {
     props.setUsers(   
       [
         {
-          id: 1, avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Flag_of_the_Republic_of_Belarus_with_the_state_emblem_2020.png/1280px-Flag_of_the_Republic_of_Belarus_with_the_state_emblem_2020.png',
-          followed: true, firstName: 'Dmitry', status: 'Hello there!', location: { country: 'Belarus', city: 'Minsk' }
+          id: 1, followed: true, firstName: 'Nickolai', status: 'Hello there!',
+          location: { country: 'Russia', city: 'Smolensk' }, avatar: './images/Nickolai_avatar.jpg'
         },
   
         {
-          id: 2, avatar: 'https://pngicon.ru/file/uploads/Flag-SShA.png',
-          followed: false, firstName: 'Alex', status: 'Hi, how are you', location: { country: 'USA', city: 'New-York' }
+          id: 2, followed: false, firstName: 'Andrey', status: 'Hi, how are you',
+          location: { country: 'Russia', city: 'Smolensk' }, avatar: './images/Andrey_avatar.jpg'
         },
   
         {
-          id: 3, avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/1500px-Flag_of_Italy.svg.png',
-          followed: true, firstName: 'Beatrice', status: 'Buongiorno :)', location: { country: 'Italy', city: 'Milano' }
+          id: 3, followed: true, firstName: 'Lyonya', status: 'Just chill...',
+          location: { country: 'Russia', city: 'Smolensk' }, avatar: './images/Lyonya_avatar.jpg'
+        },
+
+        {
+          id: 4, followed: false, firstName: 'Julia', status: 'Hi!',
+          location: { country: 'Russia', city: 'Smolensk' }, avatar: './images/Julia_avatar.jpg'
         }
       ]);
   }
   
-  let usersElements = props.users.map(u => <UserItem key={u.id} id={u.id} avatar={u.avatar} followed={u.followed} firstName={u.firstName} status={u.status} location={u.location} follow={props.follow} unfollow={props.unfollow} />);
+  let usersElements = props.users.map(u =>
+    <UserItem key={u.id} id={u.id} avatar={u.avatar} followed={u.followed}
+              firstName={u.firstName} status={u.status} location={u.location}
+              follow={props.follow} unfollow={props.unfollow} />);
   
   return (
-    <div>
-      {usersElements}
+    <div className={s.usersBlock}>
+      <div>
+        {usersElements}
+      </div>
+      <button className={s.showMoreButton}>Show more</button>
     </div>
   );
 }
