3b50e982675ac8f523e3d50106363ef0baf69b57
Nickolai <colya.polischuk@yandex.ru>
2023-10-18 22:12:16 +0300

    replaced functional component Users with class component Users (53rd lesson)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,33 +3,28 @@ import UserItem from "./UserItem/UserItem";
 import s from "./Users.module.css"
 import axios from 'axios'
 
-const Users = (props) => {
-  
-  let getUsers = () => {
-    if (props.users.length === 0) {
-      axios.get('https://social-network.samuraijs.com/api/1.0/users')
-        .then(response => {
-          props.setUsers(response.data.items);
-        });
-    }
-  }
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
 
-  let usersElements = props.users.map(u =>
-    <UserItem key={u.id} id={u.id} photos={u.photos} followed={u.followed} name={u.name}
-              status={u.status} follow={props.follow} unfollow={props.unfollow} />);
+    axios.get('https://social-network.samuraijs.com/api/1.0/users')
+      .then(response => {
+        this.props.setUsers(response.data.items);
+      });
+  }
   
-  return (
-    <div className={s.usersBlock}>
-      <div>
-        {usersElements}
-      </div>      
-      {
-        props.users.length === 0
-          ? <button className={s.getUsersButton} onClick={getUsers}>Get Users</button>
-          : <button className={s.showMoreButton}>Show more</button>
-      }
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className={s.usersBlock}>
+        <div>
+          {this.props.users.map(u =>
+            <UserItem key={u.id} id={u.id} photos={u.photos} followed={u.followed} name={u.name}
+                      status={u.status} follow={this.props.follow} unfollow={this.props.unfollow} />)}
+        </div>              
+        <button className={s.showMoreButton}>Show more</button>
+      </div>
+    ); 
+  }
+} 
 
 export default Users;
