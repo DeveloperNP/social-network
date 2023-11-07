d0abc6a8c257c4b247c5739843c7d13f91d07a4c
Nickolai <colya.polischuk@yandex.ru>
2023-11-07 21:53:08 +0300

    refactoring ProfileInfo component

--- src/components/Profile/PropfileInfo/ProfileInfo.jsx
+++ src/components/Profile/PropfileInfo/ProfileInfo.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import s from './ProfileInfo.module.css'
 import Preloader from '../../common/Preloader/Preloader';
 
+
+import userPhoto from '../../../assets/images/userPhoto.jpg'
+import magnifyingGlass from '../../../assets/images/magnifying_glass.png'
+import doNotDisturb from '../../../assets/images/do_not_disturb.png'
+import mainLink from '../../../assets/images/mainLink.png'
+import website from '../../../assets/images/website.png'
+import vk from '../../../assets/images/vk.png'
+import twitter from '../../../assets/images/twitter.png'
+import facebook from '../../../assets/images/facebook.png'
+import instagram from '../../../assets/images/instagram.png'
+import youtube from '../../../assets/images/youtube.png'
+import github from '../../../assets/images/github.png'
+
 const ProfileInfo = (props) => {
   
   if(!props.profile) {
@@ -9,18 +22,108 @@ const ProfileInfo = (props) => {
   }
   
   return (
-    <div>
+    <div className={s.profileInfoBlock}>
+      
       <div>
-        <img className={s.cover} src="./images/profile_cover.jpg" alt="cover" />
+        {props.profile.photos.large
+          ? <img className={s.avatar} src={props.profile.photos.large} alt="AVA" />
+          : <img className={s.avatar} src={userPhoto} alt="AVA" />
+        }
       </div>
+
       <div className={s.descriptionBlock}>
-        <div>
-          <img className={s.avatar} src={props.profile.photos.large} alt="AVA" />
+        <div className={s.fullName}>
+          {props.profile.fullName}
         </div>
-        <div className={s.description}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Delectus iste maiores error? Suscipit quo, maiores dolores quidem odit ducimus nulla voluptas atque. Asperiores atque veritatis aliquid magnam incidunt enim sint ut ex repellendus fugiat maxime, ad sunt rerum accusamus. Commodi nihil non similique laborum magnam possimus. Pariatur iure culpa atque?
+        <div className={s.aboutMe}>
+          {props.profile.aboutMe}
+        </div>
+        <div>
+          {props.profile.lookingForAJob
+            ? <div className={s.imgPlusTextBlock}>
+                <img className={s.smallIcon} src={magnifyingGlass} />
+                <span>I'm looking for a job</span>
+              </div>
+            : <div className={s.imgPlusTextBlock}>
+                <img className={s.smallIcon} src={doNotDisturb} />
+                <span>I'm not looking for a job</span>
+              </div>
+          }
+        </div>
+        <div className={s.lookingForAJobDescription}>
+          {props.profile.lookingForAJobDescription}
         </div>
       </div>
+
+      <div className={s.contacts}>
+        <span className={s.contactsHeader}>Contacts:</span>
+        <div className={s.linksBlock}>         
+          <div className={s.imgPlusTextBlock}>
+            <img className={s.smallIcon} src={mainLink} /> 
+            {props.profile.contacts.mainLink
+              ? <span>{props.profile.contacts.mainLink}</span>
+              : <span>-</span>
+            }
+          </div>
+        
+          <div className={s.imgPlusTextBlock}>
+            <img className={s.smallIcon} src={website}/>
+            {props.profile.contacts.website
+              ? <span>{props.profile.contacts.website}</span>
+              : <span>-</span>
+            } 
+          </div>
+
+          <div className={s.imgPlusTextBlock}>
+            <img className={s.smallIcon} src={vk}/>
+            {props.profile.contacts.vk
+              ? <span>{props.profile.contacts.vk}</span>
+              : <span>-</span>
+            } 
+          </div>
+
+          <div className={s.imgPlusTextBlock}>
+            <img className={s.smallIcon} src={twitter}/>
+            {props.profile.contacts.twitter
+              ? <span>{props.profile.contacts.twitter}</span>
+              : <span>-</span>
+            } 
+          </div>
+
+          <div className={s.imgPlusTextBlock}>
+            <img className={s.smallIcon} src={facebook}/>
+            {props.profile.contacts.facebook
+              ? <span>{props.profile.contacts.facebook}</span>
+              : <span>-</span>
+            } 
+          </div>
+
+          <div className={s.imgPlusTextBlock}>
+            <img className={s.smallIcon} src={instagram}/>
+            {props.profile.contacts.instagram
+              ? <span>{props.profile.contacts.instagram}</span>
+              : <span>-</span>
+            } 
+          </div>
+
+          <div className={s.imgPlusTextBlock}>
+            <img className={s.smallIcon} src={youtube}/>
+            {props.profile.contacts.youtube
+              ? <span>{props.profile.contacts.youtube}</span>
+              : <span>-</span>
+            } 
+          </div>
+
+          <div className={s.imgPlusTextBlock}>
+            <img className={s.smallIcon} src={github}/>
+            {props.profile.contacts.github
+              ? <span>{props.profile.contacts.github}</span>
+              : <span>-</span>
+            } 
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
