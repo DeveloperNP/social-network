a6cac3066f81cd80f3a89d4245947d9cdd1c9ae3
Nickolai <colya.polischuk@yandex.ru>
2023-11-08 23:50:45 +0300

    refacoring transition to user profile using withRouter (60th lesson)

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -3,11 +3,16 @@ import axios from 'axios'
 import Profile from './Profile';
 import { connect } from 'react-redux';
 import { setUserProfile } from '../../redux/profile-reducer';
+import withRouter from '../../ReactRouter/withRouter';
 
 class ProfileContainer extends React.Component {
   
   componentDidMount() {    
-    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/2`)
+    
+    let userID = this.props.router.params.userID;
+    if (!userID) { userID = 2; }
+
+    axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userID}`)
       .then(response => {
         this.props.setUserProfile(response.data);        
       });
@@ -22,4 +27,6 @@ let mapStateToProps = (state) => ({
   profile: state.profilePage.profile
 })
 
-export default connect(mapStateToProps, {setUserProfile})(ProfileContainer);
+let WithURLDataProfileContainer = withRouter(ProfileContainer);
+
+export default connect(mapStateToProps, {setUserProfile})(WithURLDataProfileContainer);
