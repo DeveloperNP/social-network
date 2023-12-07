da0fca1cb1c99fac4fd86cf590d8291dbc4a5206
Nickolai <colya.polischuk@yandex.ru>
2023-12-07 23:54:26 +0300

    added LoginForm using Redux Form library (75th lesson)

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import s from './Login.module.css'
+import { Field, reduxForm } from 'redux-form';
+
+const LoginForm = (props) => {
+  return(
+    <form onSubmit={props.handleSubmit}>
+      <div>        
+        <Field component={'input'} name={'login'} placeholder={'Login'} />
+      </div>
+      <div>        
+        <Field component={'input'} name={'password'} placeholder={'Password'} />
+      </div>
+      <div>        
+        <Field component={'input'} name={'rememberMe'} type={'checkbox'} />Remember me
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  );
+}
+
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
 
 const Login = (props) => {
+  const onSubmit = (formData) => {
+    console.log(formData);
+  }
+  
   return (
     <div className={s.loginBlock}>
       <h2>Login Page</h2>
+      <LoginReduxForm onSubmit={onSubmit} />
     </div>
   );
 }
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,6 +4,7 @@ import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
 import usersReducer from "./users-reducer";
 import authReducer from "./auth-reducer";
+import { reducer as formReducer } from 'redux-form'
 
 const store = configureStore({
   reducer: {
@@ -11,7 +12,8 @@ const store = configureStore({
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer
   }
 });
 
