73a5035d271c2bbd9732e8fabffa84ba6c111ec7
Nickolai <colya.polischuk@yandex.ru>
2024-01-23 01:02:37 +0300

    added unit tests for profile-reducer (89th lesson)

---
+++ src/redux/profile-reducer.test.js
@@ -0,0 +1,34 @@
+import profileReducer, { addPost } from "./profile-reducer";
+
+let state = {
+  posts: [
+    { id: 1, message: 'Forza Milan!!!', likesCount: 1899 },
+    { id: 2, message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!', likesCount: 508 },
+    { id: 3, message: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi fugiat ratione, deserunt delectus doloribus rerum. Veniam, facilis modi. Dolorem necessitatibus soluta a cumque eveniet nulla dolores consectetur. Quibusdam, perferendis suscipit!', likesCount: 287 },
+    { id: 4, message: 'The weather is fine today', likesCount: 35 },
+    { id: 5, message: 'How are you?', likesCount: 58 },
+    { id: 6, message: 'Hello, World! This is my first post ^.^', likesCount: 104 }
+  ]
+};
+
+test('new post should be added', () => {
+  // 1. test data
+  let action = addPost('test message');
+
+  // 2. some action
+  let newState = profileReducer(state, action);
+
+  // 3. expectation
+  expect(newState.posts.length).toBe(7);
+});
+
+test('new post message should be correct', () => {
+  // 1. test data
+  let action = addPost('test message');
+
+  // 2. some action
+  let newState = profileReducer(state, action);
+
+  // 3. expectation
+  expect(newState.posts[6].message).toBe('test message');
+});
