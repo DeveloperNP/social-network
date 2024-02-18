fbe17084c83c4144b3ab19c8e727e559e66d3e3e
Nickolai <colya.polischuk@yandex.ru>
2024-02-18 21:18:41 +0300

    ProfileInfo refactoring (Contacts rendering optimization)

--- src/components/Profile/PropfileInfo/ProfileInfo.jsx
+++ src/components/Profile/PropfileInfo/ProfileInfo.jsx
@@ -7,6 +7,7 @@ import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 import userPhoto from '../../../assets/images/userPhoto.jpg'
 import magnifyingGlass from '../../../assets/images/magnifying_glass.png'
 import doNotDisturb from '../../../assets/images/do_not_disturb.png'
+
 import mainLink from '../../../assets/images/mainLink.png'
 import website from '../../../assets/images/website.png'
 import vk from '../../../assets/images/vk.png'
@@ -16,17 +17,39 @@ import instagram from '../../../assets/images/instagram.png'
 import youtube from '../../../assets/images/youtube.png'
 import github from '../../../assets/images/github.png'
 
-const ProfileInfo = (props) => {
+const contacts = {
+  "mainLink": mainLink,
+  "website": website,
+  "vk": vk,
+  "twitter": twitter,
+  "facebook": facebook,
+  "instagram": instagram,
+  "youtube": youtube,
+  "github": github
+}
+
+const Contact = ({link, image}) => {
+  return <div className={s.imgPlusTextBlock}>    
+    <img className={s.smallIcon} src={image} alt='link-img' />
+    <span>{link || '-'}</span>
+  </div>
+}
+
+const ProfileItem = ({value, image}) => {
+  return <Contact link={value} image={image} />
+}
+
+const ProfileInfo = ({profile, isOwner, savePhoto, status, updateUserStatus}) => {
   
   let [avatarFocused, setAvatarFocused] = useState(false);
 
   const onMainPhotoSelected = (e) => {
     if(e.target.files.length) {
-      props.savePhoto(e.target.files[0]);
+      savePhoto(e.target.files[0]);
     }
   }
 
-  if(!props.profile) {
+  if(!profile) {
     return <Preloader />
   }
   
@@ -37,85 +60,43 @@ const ProfileInfo = (props) => {
         <div className={s.avatarBlock}
              onMouseOver={() => { setAvatarFocused(true) }}
              onMouseOut={() => { setAvatarFocused(false) }}>
-          <img className={s.avatar} src={props.profile.photos.large || userPhoto} alt="AVA" />
-          {props.isOwner && avatarFocused && <input type='file' onChange={onMainPhotoSelected} />}
+          <img className={s.avatar} src={profile.photos.large || userPhoto} alt="AVA" />
+          {isOwner && avatarFocused && <input type='file' onChange={onMainPhotoSelected} />}
         </div>
       </div>
 
       <div className={s.descriptionBlock}>
         <div className={s.fullName}>
-          {props.profile.fullName}
+          {profile.fullName}
         </div>
         <div className={s.aboutMe}>
-          {props.profile.aboutMe}
+          {profile.aboutMe}
         </div>
         <div>
-          <ProfileStatusWithHooks status={props.status} updateUserStatus={props.updateUserStatus} />
+          <ProfileStatusWithHooks status={status} updateUserStatus={updateUserStatus} />
         </div>
         <div>
-          {props.profile.lookingForAJob
-            ? <div className={s.imgPlusTextBlock}>
-                <img className={s.smallIcon} src={magnifyingGlass} />
-                <span>I'm looking for a job</span>
-              </div>
-            : <div className={s.imgPlusTextBlock}>
-                <img className={s.smallIcon} src={doNotDisturb} />
-                <span>I'm not looking for a job</span>
-              </div>
+          {profile.lookingForAJob
+            ? <ProfileItem value={"I'm looking for a job"} image={magnifyingGlass} />
+            : <ProfileItem value={"I'm not looking for a job"} image={doNotDisturb} />
           }
         </div>
         <div className={s.lookingForAJobDescription}>
-          {props.profile.lookingForAJobDescription}
+          {profile.lookingForAJobDescription}
         </div>
       </div>
 
       <div className={s.contacts}>
         <span className={s.contactsHeader}>Contacts:</span>
         <div className={s.linksBlock}>         
-          <div className={s.imgPlusTextBlock}>
-            <img className={s.smallIcon} src={mainLink} /> 
-            <span>{props.profile.contacts.mainLink || '-'}</span>
-          </div>
-        
-          <div className={s.imgPlusTextBlock}>
-            <img className={s.smallIcon} src={website}/>
-            <span>{props.profile.contacts.website || '-'}</span>
-          </div>
-
-          <div className={s.imgPlusTextBlock}>
-            <img className={s.smallIcon} src={vk}/>
-            <span>{props.profile.contacts.vk || '-'}</span>
-          </div>
-
-          <div className={s.imgPlusTextBlock}>
-            <img className={s.smallIcon} src={twitter}/>
-            <span>{props.profile.contacts.twitter || '-'}</span>
-          </div>
-
-          <div className={s.imgPlusTextBlock}>
-            <img className={s.smallIcon} src={facebook}/>
-            <span>{props.profile.contacts.facebook || '-'}</span>
-          </div>
-
-          <div className={s.imgPlusTextBlock}>
-            <img className={s.smallIcon} src={instagram}/>
-            <span>{props.profile.contacts.instagram || '-'}</span>
-          </div>
-
-          <div className={s.imgPlusTextBlock}>
-            <img className={s.smallIcon} src={youtube}/>
-            <span>{props.profile.contacts.youtube || '-'}</span>
-          </div>
-
-          <div className={s.imgPlusTextBlock}>
-            <img className={s.smallIcon} src={github}/>
-            <span>{props.profile.contacts.github || '-'}</span>
-          </div>
+          {Object.keys(profile.contacts).map(key => {
+            return <Contact link={profile.contacts[key]} image={contacts[key]} key={key} />
+          })}
         </div>
       </div>
 
     </div>
-  );
+  )
 }
 
 export default ProfileInfo;
